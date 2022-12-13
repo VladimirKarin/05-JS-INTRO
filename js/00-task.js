b754b03032e65592ab9b287afa1c917853b3251a
Vladimir Karin <Vladimir.Karin@gmail.com>
2022-12-13 13:07:46 +0200

    HW 1-st part

---
+++ js/00-task.js
@@ -0,0 +1,55 @@
+// Kintamuju inicijavimas
+// 1
+
+const firstNum = 1;
+console.log(firstNum);
+
+const secondNum = 1.79;
+console.log(secondNum);
+
+const thirdNum = 15;
+console.log(thirdNum);
+
+// 2
+
+const firstString = "Hello,";
+console.log(firstString);
+
+const secondString = "World!";
+console.log(secondString);
+
+const thirdString = "Can anyone here me?";
+console.log(thirdString);
+
+// 3
+
+const firstArray = [1, 2, 3, 4, 5];
+console.log(firstArray);
+
+const secondArray = [1.5, 2.5, 3.5, 4.5, 5.5];
+console.log(secondArray);
+
+const thirdArray = [1, 2.5, 3, 3.5, 4];
+console.log(thirdArray);
+
+// 4
+
+const firstStrArr = ["one", "two", "three", "four", "five"];
+console.log(firstStrArr);
+
+const secondStrArr = [
+  "Good day",
+  "Gutten tag",
+  "Добрый день",
+  "Laba diena",
+  "Bon giorno",
+];
+console.log(secondStrArr);
+const thirdstrArr = [
+  "Poem about arrays",
+  "Roses are red",
+  "violits are blue",
+  "I'm learning some arrays",
+  "so can you",
+];
+console.log(thirdstrArr);
