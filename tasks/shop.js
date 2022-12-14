cfe4854c46a8ec977c1fdbefd9cad1277ce2735d
Vladimir Karin <Vladimir.Karin@gmail.com>
2022-12-14 18:01:48 +0200

    shop task

--- tasks/shop.js
+++ tasks/shop.js
@@ -1,7 +1,9 @@
-const currency = "EUR";
-const prekes = [
+console.clear();
+
+const valiuta = "EUR";
+const items = [
   {
-    name: "Banana",
+    name: "Bananas",
     price: 2,
     inStock: 10,
   },
@@ -17,54 +19,78 @@ const prekes = [
     price: 1.57,
     inStock: 30,
   },
+  {
+    name: "Arbuzas",
+    price: 2,
+    inStock: 5,
+  },
+  {
+    name: "Melionas",
+    price: 20,
+    inStock: 3,
+  },
 ];
 
-function addZeroes(num) {
-  var value = Number(num);
-  var res = num.split(".");
-  if (res.length == 1 || res[1].length < 3) {
-    value = value.toFixed(2);
+function shop(goods, currency) {
+  console.log("MUSU PARDUOTUVE:");
+  console.log("----------------------------------------------------");
+  let totalPrice = 0;
+  for (let i = 0; i < items.length; i++) {
+    console.log(
+      `${items[i].name} kainuoja ${items[i].price.toFixed(
+        2
+      )} ${valiuta} ir turime ju ${items[i].inStock} vienetu.`
+    );
+    totalPrice += items[i].price * items[i].inStock;
   }
-  return value;
+  console.log("-----------------------------------------------------");
+  console.log(`Viso asortimento kaina: ${totalPrice.toFixed(2)} ${valiuta}.`);
 }
 
-function shop() {}
+shop(items, valiuta);
 
-shop(prekes, currency);
+// function shop(items, currency) {
+//   const valiuta = " EUR";
+//   const quantity = " vnt.";
+//   const shopName = "Musu Parduotuve";
+//   console.log(shopName);
+//   console.log("--------------------");
+//
+// }
 
-const valiuta = " EUR";
-const quantity = " vnt.";
+// shop(items, valiuta);
 
-// Item names
-const prekes1 = prekes[0].name;
-const prekes2 = prekes[1].name;
-const prekes3 = prekes[2].name;
-console.log(prekes1);
-console.log(prekes2);
-console.log(prekes3);
+// // Item names
+// const prekes1 = prekes[0].name;
+// const prekes2 = prekes[1].name;
+// const prekes3 = prekes[2].name;
+// console.log(prekes1);
+// console.log(prekes2);
+// console.log(prekes3);
 
-// Item cost
-const currency1 = prekes[0].price + valiuta;
-const currency2 = prekes[1].price + valiuta;
-const currency3 = prekes[2].price + valiuta;
-console.log(currency1);
-console.log(currency2);
-console.log(currency3);
+// // Item cost
+// const currency1 = prekes[0].price + valiuta;
+// const currency2 = prekes[1].price + valiuta;
+// const currency3 = prekes[2].price + valiuta;
+// console.log(currency1);
+// console.log(currency2);
+// console.log(currency3);
 
-// Item quantity
-const inStock1 = prekes[0].inStock + quantity;
-const inStock2 = prekes[0].inStock + quantity;
-const inStock3 = prekes[0].inStock + quantity;
-console.log(inStock1);
-console.log(inStock2);
-console.log(inStock3);
+// // Item quantity
+// const inStock1 = prekes[0].inStock + quantity;
+// const inStock2 = prekes[0].inStock + quantity;
+// const inStock3 = prekes[0].inStock + quantity;
+// console.log(inStock1);
+// console.log(inStock2);
+// console.log(inStock3);
 
-console.log("<Musu Parduotuve:>");
-console.log("_______________________");
-console.log("1) Bananas kainuoja 2.00 EUR ir turime ju 10 vnt");
-console.log("2) Agurkas kainuoja 3.00 EUR ir turime ju 10 vnt");
-console.log("3) Bananas kainuoja 1.57 EUR ir turime ju 30 vnt");
-console.log("_______________________");
-console.log("Viso asortimento kaina: 97.1");
+// console.log("<Musu Parduotuve:>");
 
-console.log(prekes.name);
+// console.log("_______________________");
+// console.log("1) Bananas kainuoja 2.00 EUR ir turime ju 10 vnt");
+// console.log("2) Agurkas kainuoja 3.00 EUR ir turime ju 10 vnt");
+// console.log("3) Bananas kainuoja 1.57 EUR ir turime ju 30 vnt");
+// console.log("_______________________");
+// console.log("Viso asortimento kaina: 97.1");
+
+// console.log(prekes.name);
