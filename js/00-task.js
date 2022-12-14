626b571d89d38198e9c423da9af37974349347b1
Vladimir Karin <Vladimir.Karin@gmail.com>
2022-12-14 10:19:50 +0200

    if

--- js/00-task.js
+++ js/00-task.js
@@ -1,5 +1,6 @@
 // Kintamuju inicijavimas
 // 1
+console.log("__________1.1__________");
 
 const firstNum = 1;
 console.log(firstNum);
@@ -11,17 +12,19 @@ const thirdNum = 15;
 console.log(thirdNum);
 
 // 2
+console.log("__________1.2__________");
 
-const firstString = "Hello,";
+const firstString = "Hello there,";
 console.log(firstString);
 
-const secondString = "World!";
+const secondString = "Little world!";
 console.log(secondString);
 
 const thirdString = "Can anyone here me?";
 console.log(thirdString);
 
 // 3
+console.log("__________1.3__________");
 
 const firstArray = [1, 2, 3, 4, 5];
 console.log(firstArray);
@@ -33,6 +36,7 @@ const thirdArray = [1, 2.5, 3, 3.5, 4];
 console.log(thirdArray);
 
 // 4
+console.log("__________1.4__________");
 
 const firstStrArr = ["one", "two", "three", "four", "five"];
 console.log(firstStrArr);
@@ -46,28 +50,31 @@ const secondStrArr = [
 ];
 console.log(secondStrArr);
 
-const thirdstrArr = [
+const thirdStrArr = [
   "Poem about arrays",
   "Roses are red",
   "violets are blue",
   "I'm learning some arrays",
   "so can you",
 ];
-console.log(thirdstrArr);
+console.log(thirdStrArr);
 
 // VEIKSMAI SU KINTAMAISIAIS
 
 // 1
+console.log("__________2.1__________");
 
 const sumNum = firstNum + secondNum + thirdNum;
 console.log(sumNum);
 
 // 2
+console.log("__________2.2__________");
 
 const sumString = firstString + " " + secondString + " " + thirdString;
 console.log(sumString);
 
 // 3
+console.log("__________2.3__________");
 
 const firstArrayType = typeof firstArray;
 console.log(firstArrayType);
@@ -83,6 +90,8 @@ value += firstArray[4];
 console.log("Value of firstArray's all numbers sum: " + value);
 
 // 4
+console.log("__________2.4__________");
+
 const thirdArrayType = typeof thirdstrArr;
 console.log(thirdArrayType);
 
@@ -90,9 +99,308 @@ let secondValue = "From end to start with comas and spaces: ";
 const separator = ", ";
 const ending = ".";
 
-secondValue += thirdstrArr[4] + separator;
-secondValue += thirdstrArr[3] + separator;
-secondValue += thirdstrArr[2] + separator;
-secondValue += thirdstrArr[1] + separator;
-secondValue += thirdstrArr[0] + separator;
+secondValue += thirdStrArr[4] + separator;
+secondValue += thirdStrArr[3] + separator;
+secondValue += thirdStrArr[2] + separator;
+secondValue += thirdStrArr[1] + separator;
+secondValue += thirdStrArr[0] + separator;
 console.log(secondValue);
+
+// Kintamuju Palygininmas
+
+// 3.1.1
+console.log("__________3.1.1__________");
+
+if (firstNum > secondNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+if (secondNum > thirdNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+if (firstNum > thirdNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+// 3.1.2
+console.log("__________3.1.2__________");
+
+if (firstNum < secondNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+if (thirdNum < firstNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+if (secondNum < thirdNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+// 3.1.3
+console.log("__________3.1.3__________");
+
+if (firstNum === secondNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+if (thirdNum === firstNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+if (secondNum === thirdNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+// 3.1.4
+console.log("__________3.1.4__________");
+
+if (firstNum !== secondNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+if (thirdNum !== firstNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+if (secondNum !== thirdNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+// 3.1.5
+console.log("__________3.1.5__________");
+
+if (firstNum >= secondNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+if (thirdNum >= firstNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+if (secondNum >= thirdNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+// 3.1.6
+console.log("__________3.1.6__________");
+
+if (firstNum <= secondNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+if (thirdNum <= firstNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+if (secondNum <= thirdNum) {
+  console.log(" Pomidoras ");
+} else {
+  console.log(" Bandykite kita karta ");
+}
+
+// 3.2
+console.log("__________3.2.0__________");
+
+console.log("Total: " + firstString.length + " letters");
+
+console.log("Total: " + secondString.length + " letters");
+
+console.log("Total: " + thirdString.length + " letters");
+
+// 3.3
+// 1
+console.log("__________3.3.1__________");
+
+if (firstString.length > secondString.length) {
+  console.log(firstString + " -is longer/bigger than- " + secondString);
+} else {
+  console.log(firstString + " -is shorter/smaller than- " + secondString);
+}
+
+if (firstString.length > thirdString.length) {
+  console.log(firstString + " -is longer/bigger than- " + thirdString);
+} else {
+  console.log(firstString + " -is shorter/smaller than- " + secondString);
+}
+
+if (thirdString.length > secondString.length) {
+  console.log(thirdString + " -is longer/bigger than- " + secondString);
+} else {
+  console.log(thirdString + " -is shorter/smaller than- " + secondString);
+}
+
+// 2
+console.log("__________3.3.2__________");
+
+if (firstString.length < secondString.length) {
+  console.log(firstString + " -is shorter/smaller than- " + secondString);
+} else {
+  console.log(firstString + " -is longer/bigger than- " + secondString);
+}
+
+if (firstString.length < thirdString.length) {
+  console.log(firstString + " -is shorter/smaller than- " + thirdString);
+} else {
+  console.log(firstString + " -is longer/bigger than- " + secondString);
+}
+
+if (thirdString.length < secondString.length) {
+  console.log(thirdString + " -is shorter/smaller than- " + secondString);
+} else {
+  console.log(thirdString + " -is longer/bigger than- " + secondString);
+}
+
+// 3
+console.log("__________3.3.3__________");
+
+if (firstString.length === secondString.length) {
+  console.log(firstString + " -is same length as- " + secondString);
+} else {
+  console.log(firstString + " -is not the same length as- " + secondString);
+}
+
+if (firstString.length === thirdString.length) {
+  console.log(firstString + " -is same length as- " + thirdString);
+} else {
+  console.log(firstString + " -is not the same length as- " + secondString);
+}
+
+if (thirdString.length === secondString.length) {
+  console.log(thirdString + " -is same length as- " + secondString);
+} else {
+  console.log(thirdString + " -is not the same length as- " + secondString);
+}
+
+// 4
+console.log("__________3.3.4__________");
+
+if (firstString.length !== secondString.length) {
+  console.log(firstString + " -is not the same length as- " + secondString);
+} else {
+  console.log(firstString + "  -is same length as-  " + secondString);
+}
+
+if (firstString.length !== thirdString.length) {
+  console.log(firstString + " -is not the same length as- " + thirdString);
+} else {
+  console.log(firstString + "  -is same length as-  " + secondString);
+}
+
+if (thirdString.length !== secondString.length) {
+  console.log(thirdString + " -is not the same length as- " + secondString);
+} else {
+  console.log(thirdString + " -is same length as- " + secondString);
+}
+
+// 5
+console.log("__________3.3.5__________");
+
+if (firstString.length >= secondString.length) {
+  console.log(
+    firstString + " -is bigger/larger or same length as- " + secondString
+  );
+} else {
+  console.log(
+    firstString + "  -is NOT bigger/larger or same length as-  " + secondString
+  );
+}
+
+if (firstString.length >= thirdString.length) {
+  console.log(
+    firstString + " -is bigger/larger or same length as- " + thirdString
+  );
+} else {
+  console.log(
+    firstString + "  -is NOT bigger/larger or same length as-  " + secondString
+  );
+}
+
+if (thirdString.length >= secondString.length) {
+  console.log(
+    thirdString + " -is bigger/larger or same length as- " + secondString
+  );
+} else {
+  console.log(
+    thirdString + " -is NOT bigger/larger or same length as- " + secondString
+  );
+}
+
+// 6
+console.log("__________3.3.6__________");
+
+if (firstString.length <= secondString.length) {
+  console.log(
+    firstString + " -is smaller/shorter or same length as- " + secondString
+  );
+} else {
+  console.log(
+    firstString +
+      "  -is NOT smaller/shorter or same length as-  " +
+      secondString
+  );
+}
+
+if (firstString.length <= thirdString.length) {
+  console.log(
+    firstString + " -is smaller/shorter or same length as- " + thirdString
+  );
+} else {
+  console.log(
+    firstString +
+      "  -is NOT smaller/shorter or same length as-  " +
+      secondString
+  );
+}
+
+if (thirdString.length <= secondString.length) {
+  console.log(
+    thirdString + " -is smaller/shorter or same length as- " + secondString
+  );
+} else {
+  console.log(
+    thirdString + " -is NOT smaller/shorter or same length as- " + secondString
+  );
+}
+
+// 4
+if (thirdArray.length > thirdStrArr.length) {
+  console.log(thirdArray + " -is biggerlonger than - " + thirdStrArr);
+} else {
+  console.log(thirdArray + "  -is NOT biggerlonger than -  " + thirdStrArr);
+}
--- js/09-if.js
+++ js/09-if.js
@@ -14,6 +14,11 @@ IF () {} else IF () {}
 IF () {} else IF () {} else {}
 IF () {} else IF () {} else IF () {}
 IF () {} else IF () {} else IF () {} else IF () {} else {}..
+
+LOGINIAI OPERATORIAI:
+&& - AND - turi tenkinti visas salygas
+|| - OR  - turi tenkinti bent viena salyga
+
 */
 const a = 7;
 const b = 5;
@@ -74,4 +79,59 @@ if (5 === 5) {
 
 if (5 !== 3) {
   console.log("Ar ne lygu ?" + "-Taip");
+}
+
+console.clear();
+console.log("Clear: 80");
+
+// Code Nesting
+
+const day = 1;
+
+if (day === 1) {
+  console.log("pirmadienis");
+} else if (day === 2) {
+  console.log("antradienis");
+} else if (day === 3) {
+  console.log("treciadienis");
+} else if (day === 4) {
+  console.log("ketvirtadienis");
+} else if (day === 5) {
+  console.log("penktadienis");
+} else if (day === 6) {
+  console.log("sestadienies");
+} else if (day === 7) {
+  console.log("sekmadienies");
+} else {
+  console.log("tokia diena savaiteje neegzistoja: :( ");
+}
+
+// Atrakcionu parkas
+
+const parkasDirba = true;
+const bijauAukscio = false;
+
+// if (parkasDirba) {
+//   if (bijauAukscio) {
+//   console.log("Einu i parka, nes dirba");
+// } else {
+//   console.log("Parkas ne dirba. Bandykite kita karta.");
+// } else {
+//   if (bijauAukscio) {
+//     console.log("Parkas nedirba - man pasiseke - liksiu sausomis kelnemis.");
+//   } else {
+//     console.log("Parkas nedirba. Man liudna, nes norejau nueiti:( ");
+//   }
+// }
+
+if (parkasDirba && bijauAukscio) {
+  console.log("Maaaaamaaaaa!");
+} else if (parkasDirba && !bijauAukscio) {
+  console.log("Einu i parka - bus smagu");
+} else if (!parkasDirba && bijauAukscio) {
+  console.log("parkas nedirba- man pasiseke - liksiu sausomis kelnemis");
+} else if (!parkasDirba && !bijauAukscio) {
+  console.log("Parkas ne dirba. Man liudna nes labai norejau nueiti");
+} else {
+  console.log("Neimanomas pasiekti atvejis!");
 }
