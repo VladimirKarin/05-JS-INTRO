d851e97fcf8f001279db2f24907c56a7b5c0b2d7
Vladimir Karin <Vladimir.Karin@gmail.com>
2022-12-19 20:38:48 +0200

    CodeWars

---
+++ CodeWars/stringDebug.js
@@ -0,0 +1,8 @@
+function main(verb, noun) {
+  const one = " ";
+  const two = " ";
+  return one + two;
+}
+
+console.log(main("take ", "item"), "--->", "take item");
+console.log(main("use ", "sword"), "--->", "use sword");
