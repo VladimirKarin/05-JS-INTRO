90c85f253cb1845f8eb8988418e9e631886e63b6
Vladimir Karin <Vladimir.Karin@gmail.com>
2022-12-22 15:07:39 +0200

    complex ternary

--- js/11-ternary.js
+++ js/11-ternary.js
@@ -36,3 +36,37 @@ console.log(wallet);
 const msgWin = lotteryWin;
 const msg = msgWin ? "Sveikinu" : "Ups...";
 console.log(msg);
+
+//COMPLEX TERNARY
+
+const a = 0 < 1 ? 2 : 3;
+console.log(2);
+
+// if (0 < 1) {
+//  a = 2;
+// } else {
+//  a = 3;
+// }
+
+const b = 0 > 1 ? 2 : 3;
+console.log(b);
+
+const c = 0 === 1 ? 2 : 3;
+console.log(c);
+
+const d = 0 < 1 ? 2 : 3 ? 4 : 5;
+console.log(d);
+
+const e = 0 > 1 ? 2 : 3 ? 4 : 5;
+console.log(e);
+
+const f = 0 <
+// if (0 < 1) {
+//   a = 2;
+// } else {
+//   if (3) {
+//     a = 4;
+//   } else {
+//     a = 5;
+//   }
+// }
