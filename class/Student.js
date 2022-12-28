9c1be6a3fdb7144b9f30a984f8d3dd973c869189
Vladimir Karin <Vladimir.Karin@gmail.com>
2022-12-28 13:53:02 +0200

    Student marks average

--- class/Student.js
+++ class/Student.js
@@ -6,9 +6,32 @@ class Student {
     this.shoes = 40;
     this.marks = [];
   }
-}
+  //Metodas itraukiantis nauja pazymi
+  addMark(mark) {
+    this.marks.push(mark);
+  }
 
-export default Student;
+  marksAverage() {
+    if (this.marks.length === 0) {
+      return "neiskaita";
+    }
+
+    // const correctMarks = this.marks
+    //   .filter((m) => m > 0)
+    //   .filter((m) => m <= 10)
+    //   .filter((m) => m % 1 === 0);
+
+    const correctMarks = this.marks.filter(
+      (m) => m >= 1 && m <= 10 && m % 1 === 0
+    );
+
+    console.log(correctMarks);
+    return correctMarks.reduce((t, m) => t + m, 0) / this.marks.length;
+  }
+}
+export { Student };
+
+// export default Student;
 
 //Alternativa
 // module.exports = Student;
--- class/index.js
+++ class/index.js
@@ -1,16 +1,34 @@
 console.clear();
 console.log("Class...");
 
-import Student from "./Student.js";
+import { Student } from "./Student.js";
+//Jeigu is vieno failo kelis dalykus - (import {Student, Teacher} from "./blablabla.js")
 
 //Alternativa
 // const Student = require("./Student");
 
 const petras = new Student("Petras", 2000);
 const maryte = new Student("Maryte", 1980);
+const jonas = new Student("Jonas", 1990);
 
-console.log(petras);
-console.log(petras, petras.name, petras.marks);
-console.log(maryte);
-console.log(maryte, maryte.name, maryte.marks);
-console.log(Student);
+petras.addMark(10);
+petras.addMark(0);
+petras.addMark(2);
+petras.addMark(22);
+petras.addMark(-22);
+petras.addMark(8);
+petras.addMark(3.14);
+petras.addMark(-3.14);
+petras.addMark(4);
+petras.addMark(6);
+
+maryte.addMark(9);
+maryte.addMark(6);
+
+console.log(petras.name, petras.marks);
+console.log(maryte.name, maryte.marks);
+console.log(jonas.name, jonas.marks);
+
+console.log(petras.name, petras.marksAverage());
+console.log(maryte.name, maryte.marksAverage());
+console.log(jonas.name, jonas.marksAverage());
