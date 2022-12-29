83664e3879a41c4f3f317ee339a2621b9b743a91
Vladimir Karin <Vladimir.Karin@gmail.com>
2022-12-29 15:20:33 +0200

    kioskas

--- shop/shop.js
+++ shop/shop.js
@@ -1,14 +1,55 @@
 class Shop {
-  constructor(item, cost) {
-    this.name = item;
-    this.cost = cost;
+  constructor(name, currency) {
+    this.name = name;
+    this.currency = currency;
+    this.products = [];
   }
 
   intro() {
-    console.log(
-      'Hi, we are "Meskiuko kioskas".\nUse .items() method to get list of items to purchase.\nUse .order() method to get your order details.'
-    );
+    const res = `Hi, we are "${this.name}".\nUse .items() method to get list of items to purchase.\nUse .order() method to get your order details.`;
+
+    console.log(res);
+    return res;
+  }
+
+  formattedPrice(price) {
+    return `${(price / 100).toFixed(2)} ${this.currency}`;
+  }
+
+  formattedName(name) {
+    return name[0].toUpperCase() + name.slice(1);
+  }
+
+  addItem(product, price) {
+    this.products.push({
+      name: product,
+      price: price,
+    });
+
+    const res = `"${this.name}" sells ${product} for ${this.formattedPrice(
+      price
+    )} now!`;
+
+    console.log(res);
+    return res;
+  }
+
+  items() {
+    const title = `Items for sale at "${this.name}":`;
+    const line = `====================`;
+    let list = "";
+    let i = 0;
+
+    for (const { name, price } of this.products) {
+      list += `${++i}) ${this.formattedName(name)} - ${this.formattedPrice(
+        price
+      )};\n`;
+    }
+
+    const res = `${title}\n${line}\n${list}${line}`;
+
+    console.log(res);
+    return res;
   }
 }
-
 export { Shop };
