760f95bb8b5c0071e3b2deb1343ee107e539c0b9
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-01-11 15:03:56 +0200

    Math methods

---
+++ js/21-math.js
@@ -0,0 +1,51 @@
+console.clear();
+// Math biblioteka
+
+console.log("----------");
+console.log("Math.floor("); //apvalyna i mazesne puse
+console.log(Math.floor(3.9999));
+console.log(Math.floor(3.5));
+console.log(Math.floor(3.000001));
+
+console.log("----------");
+console.log("Math.ceil("); //apvalyna i dydesne puse
+console.log(Math.ceil(3.9999));
+console.log(Math.ceil(3.5));
+console.log(Math.ceil(3.000001));
+
+console.log("----------");
+console.log("Math.round("); //apvalyna i dydesne puse
+console.log(Math.round(3.9999));
+console.log(Math.round(3.5));
+console.log(Math.round(3.4449));
+console.log(Math.round(3.000001));
+
+console.log("----------");
+console.log("Math.trunc("); //apvalyna i dydesne puse
+console.log(Math.trunc(3.9999));
+console.log(Math.trunc(3.5));
+console.log(Math.trunc(3.000001));
+
+console.log("----------");
+console.log("Math.min("); //randa maziausia skaiciu
+console.log(Math.min(3.9999));
+console.log(Math.min(3.5));
+console.log(Math.min(3.000001));
+
+console.log("----------");
+console.log("Math.max("); //randa dydesni skaiciu
+console.log(Math.max(3.9999));
+console.log(Math.max(3.5));
+console.log(Math.max(3.000001));
+
+console.log("----------");
+console.log("Math.abs("); //apvalyna i dydesne puse
+console.log(Math.abs(3.9999));
+console.log(Math.abs(3.5));
+console.log(Math.abs(3.000001));
+
+console.log("----------");
+console.log("Math.random("); //apvalyna i dydesne puse [0..1 -> [0..0.999999]]
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
